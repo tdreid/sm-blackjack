1286f075f851dcdf92e6d3684f9a4fe7f51ca52b
tdreid <trevor.d.reid@gmail.com>
2018-10-24 00:24:27 +0000

    Configure state machine factory in sm-blackjack.js

---
+++ sm-blackjack.js
@@ -0,0 +1,25 @@
+const StateMachine = require('javascript-state-machine');
+
+const Blackjack = StateMachine.factory({
+  init: 'wagering',
+  transitions: [
+    { name: 'player_placed_bet', from: 'wagering', to: 'dealing' },
+    { name: 'dealt', from: 'dealing', to: 'player_turn' },
+    { name: 'player_hit', from: 'player_turn', to: 'player_turn' },
+    { name: 'player_hit', from: 'player_turn', to: 'loss' },
+    { name: 'player_stood', from: 'player_turn', to: 'dealer_turn' },
+    { name: 'dealer_finished', from: 'dealer_turn', to: 'win' },
+    { name: 'dealer_finished', from: 'dealer_turn', to: 'push' },
+    { name: 'dealer_finished', from: 'dealer_turn', to: 'loss' },
+    { name: 'player_quit', from: 'loss', to: 'quit' },
+    { name: 'player_quit', from: 'push', to: 'quit' },
+    { name: 'player_quit', from: 'win', to: 'quit' },
+    { name: 'player_continue', from: 'loss', to: 'atm' },
+    { name: 'player_continue', from: 'loss', to: 'wagering' },
+    { name: 'player_continue', from: 'push', to: 'wagering' },
+    { name: 'player_continue', from: 'win', to: 'wagering' },
+    { name: 'player_withdrew_cash', from: 'atm', to: 'wagering' }
+  ]
+});
+
+module.exports = Blackjack;
