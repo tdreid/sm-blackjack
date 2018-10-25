d9611fe2abec88ff22ec6e4dbe8330b2758e1e99
tdreid <trevor.d.reid@gmail.com>
2018-10-25 23:37:15 +0000

    Add a test for the allStates() method

--- sm-blackjack.spec.js
+++ sm-blackjack.spec.js
@@ -34,4 +34,22 @@ describe('Blackjack', function() {
       assert.deepEqual(new Blackjack().allTransitions(), expectedTransitions);
     });
   });
+  describe('allStates method', function() {
+    it('should list all the expected states and nothing else', function() {
+      const expectedStates = [
+        'none',
+        'wagering',
+        'dealing',
+        'player_turn',
+        'loss',
+        'dealer_turn',
+        'win',
+        'push',
+        'quit',
+        'atm'
+      ];
+
+      assert.deepEqual(new Blackjack().allStates(), expectedStates);
+    });
+  });
 });
